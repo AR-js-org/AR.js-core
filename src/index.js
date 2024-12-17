4675031dee528ab8d11ec3804ad26849e6181f2c
Walter Perdan <github@kalwaltart.it>
2024-12-17 20:40:22 +0100

    export new classes in the index.js - Context instead of ArToolkitContext

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import Context from "./arjs-context";
 import Source from "./arjs-source";
 import Profile from "./arjs-profile";
 import Anchor from "./new-api/arjs-anchor";
 import Session from "./new-api/arjs-session";
+import { AnchorDebugUI } from "./new-api/arjs-debugui";
 import { SessionDebugUI } from "./new-api/arjs-debugui";
 
-export { Anchor, Source, Profile, Session, SessionDebugUI };
+export default { Anchor, Context, Source, Profile, Session, AnchorDebugUI, SessionDebugUI };
